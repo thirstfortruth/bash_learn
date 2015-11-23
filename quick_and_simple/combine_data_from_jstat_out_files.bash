a1bf6fbdc61be50d6b6e4e9f52edf1bc464cea4b
thirstfortruth <thirstfortruth@yandex.ru>
2015-11-23 23:29:01 +0300

    combine data from jstat out file
    required to get output for different out files to one - filenames are
    created based on java process options going as the 1st line in each out
    file.
    Created as I'm too lazy to make it manually.

---
+++ quick_and_simple/combine_data_from_jstat_out_files.bash
@@ -0,0 +1,9 @@
+for file in `ls -latr|awk {'print $9'}|grep '_pid_stats'`;do
+	out_file="`cat ${file} |head -n 1 |tee >(sed 's/ /\n/g'|grep 'Dweblogic.Name'|cut -d"=" -f2)|cut -d" " -f2|grep -v Server`.fill_data.log"
+	if [ -w ${out_file} ];then
+		cat file >> ${out_file};
+	else
+		cat file > ${out_file};
+	fi
+done;
+
