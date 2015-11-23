3c111664dc448731b2015f724aefb0bbaa2b3d00
thirstfortruth <thirstfortruth@yandex.ru>
2015-11-23 23:38:22 +0300

    minor changes

--- quick_and_simple/combine_data_from_jstat_out_files.bash
+++ quick_and_simple/combine_data_from_jstat_out_files.bash
@@ -1,3 +1,4 @@
+#!/bin/bash
 for file in `ls -latr|awk {'print $9'}|grep '_pid_stats'`;do
 	out_file="`cat ${file} |head -n 1 |tee >(sed 's/ /\n/g'|grep 'Dweblogic.Name'|cut -d"=" -f2)|cut -d" " -f2|grep -v Server`.fill_data.log"
 	if [ -w ${out_file} ];then
