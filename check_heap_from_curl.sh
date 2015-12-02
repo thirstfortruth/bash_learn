c84a95b7cc8e71b983e78443be62e37fb1105fef
thirstfortruth <thirstfortruth@yandex.ru>
2015-12-02 23:05:56 +0300

    Working version.
    few checks added for input arguments.

--- check_heap_from_curl.sh
+++ check_heap_from_curl.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
+#script is written to check heap size with help of app metric screen
+#adapted for usage as Nagios plugin
+#02.12.2015
 declare -i CRITICAL_THRESHOLD;
 declare -i WARNING_THRESHOLD;
 declare -r DEFAULT_WARNING_THRESHOLD=93;
 declare -r DEFAULT_CRITICAL_THRESHOLD=96;
 declare -r OPTIONS='u:w:c:h';
+declare -r OTHERS_CODE=3;
+declare -r CRITICAL_CODE=2;
+declare -r WARNING_CODE=1;
+declare -r OK_CODE=0;
 #usage
 USAGE="$0 -u <url> [-h -w <n> -c <n>] \n
 \n
-\t	where:\n
-\t	-u\t	define url to execute\n
-\t	\t	optional:\n
+\t	where:\n\n
+\t	-u <url>\turl to execute\n\n
+\t	optional:\n\n
 \t	-h\t	show this help\n
-\t	-w\t	warning theshold  % (default=${DEFAULT_WARNING_THRESHOLD})\n
-\t	-c\t	critical theshold % (default=${DEFAULT_CRITICAL_THRESHOLD})\n
+\t	-w <n>\t	warning theshold  n% (default=${DEFAULT_WARNING_THRESHOLD})\n
+\t	-c <n>\t	critical theshold n% (default=${DEFAULT_CRITICAL_THRESHOLD})\n
 ";
 #parse script input parameters
 parse_arguments () {
@@ -30,35 +37,54 @@ parse_arguments () {
 		  ;;
 		h)
 		  echo -e $USAGE >&1;
-		  exit 0;
+		  exit ${OK_CODE};
 		  ;;
 		\?)
-		  echo "Invalid option: -$OPTARG" >&2;
+		  echo -e "\nERROR: invalid option: -$OPTARG\n" >&2;
 		  echo -e $USAGE >&2;
-		  exit 1;
+		  exit ${OTHERS_CODE};
 		  ;;
 		:)
-		  echo "Option -$OPTARG requires an argument." >&2;
+		  echo -e "\nERROR: option -$OPTARG requires an argument.\n" >&2;
 		  echo -e $USAGE >&2;
-		  exit 1;
+		  exit ${OTHERS_CODE};
 		  ;;
 	  esac
 	done;
-	#set up default values if none defined
-	WARNING_THRESHOLD=${WARNING_THRESHOLD:=$DEFAULT_WARNING_THRESHOLD};
-	CRITICAL_THRESHOLD=${CRITICAL_THRESHOLD:=$DEFAULT_CRITICAL_THRESHOLD};
+	#check if both -w and -c optiones defined
+	if [ -z ${WARNING_THRESHOLD} ] && [ ! -z ${CRITICAL_THRESHOLD} ]
+	then 
+		echo -e "\nERROR: please define warning theshold!\n";
+		echo -e $USAGE >&2;
+		exit ${OTHERS_CODE};
+	elif [ -z ${CRITICAL_THRESHOLD} ] && [ ! -z ${WARNING_THRESHOLD} ]
+	then
+		echo -e "\nERROR: please define critical theshold!\n";
+		echo -e $USAGE >&2;
+		exit ${OTHERS_CODE};		
+	else
+		#set up default values if none defined
+		WARNING_THRESHOLD=${WARNING_THRESHOLD:=$DEFAULT_WARNING_THRESHOLD};
+		CRITICAL_THRESHOLD=${CRITICAL_THRESHOLD:=$DEFAULT_CRITICAL_THRESHOLD};
+		#check if critical threshold is greater than warning
+		if [ ${WARNING_THRESHOLD} -ge ${CRITICAL_THRESHOLD} ]
+		then
+			echo -e "\nERROR: warning threshold(${WARNING_THRESHOLD}%) is greater than critical theshold(${CRITICAL_THRESHOLD}%)!\n";
+			exit ${OTHERS_CODE};
+		fi;
+	fi
 	#check mandatory options
 	if [ -z ${URL} ];then
-		echo -e "ERROR: Url is not set up." >&2;
+		echo -e "\nERROR: Url is not set up!\n" >&2;
 		echo -e $USAGE >&2;
-		exit 1;
+		exit ${OTHERS_CODE};
 	fi;
 }
 #get integer percentage of argument1 compared to argument2
 calculate (){
 	if [ $# -ne 2 ];then
 		echo "ERROR: $0 wrong number of input parameters" >&2;
-		exit 1
+		exit ${OTHERS_CODE};
 	fi;
 	current_value=$1;
 	max_value=$2;
@@ -67,8 +93,8 @@ calculate (){
 #get required heap values from curl responce
 get_heap_values () {
 	if [ $# -ne 1 ];then
-		echo "ERROR: $0 wrong number of input parameters" >&2;
-		exit 1;
+		echo "ERROR: get_heap_values: wrong number of input parameters" >&2;
+		exit ${OTHERS_CODE};
 	fi;
 	L_URL=$1;
 	heap_values=$(curl -s ${L_URL} |sed 's/,/\n/g'|egrep -e 'jvm.memory.heap.(max|used)');
@@ -79,16 +105,21 @@ get_heap_values () {
 #generate message depending on input percent and configured threshold
 validate_percent (){
 	if [ $# -ne 1 ];then
-		echo "ERROR: $0 wrong number of input parameters" >&2;
-		exit 1;
+		echo "ERROR: validate_percent: wrong number of input parameters" >&2;
+		exit ${OTHERS_CODE};
 	fi;
 	L_HEAP_USAGE_PERCENT=$1;
 	if [ ${L_HEAP_USAGE_PERCENT} -ge ${WARNING_THRESHOLD} ] && [ ${L_HEAP_USAGE_PERCENT} -lt ${CRITICAL_THRESHOLD} ]
 	then
 		echo "WARNING: Heap usage is ${L_HEAP_USAGE_PERCENT}%!" >&1;
+		exit ${WARNING_CODE};
 	elif [ ${L_HEAP_USAGE_PERCENT} -ge ${CRITICAL_THRESHOLD} ]
 	then
 		echo "CRITICAL: Heap usage is ${L_HEAP_USAGE_PERCENT}%!" >&1;
+		exit ${CRITICAL_CODE};
+	else
+		echo "OK: Heap usage is ${L_HEAP_USAGE_PERCENT}%!" >&1;
+		exit ${OK_CODE};
 	fi
 }
 #begin processing
@@ -96,12 +127,3 @@ parse_arguments $@;
 HEAP_VALUES=$(get_heap_values $URL);
 HEAP_USAGE_PERCENT=$(calculate ${HEAP_VALUES});
 validate_percent ${HEAP_USAGE_PERCENT};
-
-
-
-
-
-
-
-
-
